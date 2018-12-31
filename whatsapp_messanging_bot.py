8ff498e70b96ac47ed88585c564fb997d3c856ca
Kush Choudhary <kushchoudhary8@gmail.com>
2018-12-31 19:01:12 +0530

    Whatsapp_messanging_bot_version1
    This is the most basic version which sends number of messages to a contact or group as you wish up-to infinity.

---
+++ whatsapp_messanging_bot.py
@@ -0,0 +1,24 @@
+## VERSION1
+## WORKING CHECKED ON 31-12-2018
+## Be sure that your chrome driver is upto dated
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of user or group : ')
+msg = input('Enter your message : ')
+count = int(input('Enter the count : '))
+
+input('Enter anything after scanning QR code')
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_class_name('_2S1VP')
+
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_class_name('_35EW6')
+    button.click()
