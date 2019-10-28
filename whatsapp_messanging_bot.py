7231f01533ac16b127e29c968d3c5e785d89f55c
Kush Choudhary <kushchoudhary8@gmail.com>
2019-10-28 16:21:13 +0530

    bug fix

--- whatsapp_messanging_bot.py
+++ whatsapp_messanging_bot.py
@@ -16,9 +16,9 @@ input('Enter anything after scanning QR code')
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 
-msg_box = driver.find_element_by_class_name('_2S1VP')
+msg_box = driver.find_element_by_class_name('_3u328')
 
 for i in range(count):
     msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_35EW6')
+    button = driver.find_element_by_class_name('_3M-N-')
     button.click()
