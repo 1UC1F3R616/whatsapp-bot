34f3c94e9e9c5e5ebadbc4fbe36f4a4d42117f9d
Kush Choudhary <kushchoudhary8@gmail.com>
2019-10-28 16:21:28 +0530

    bug fix

--- whatsapp_messanging_botV2.py
+++ whatsapp_messanging_botV2.py
@@ -24,7 +24,7 @@ input('Enter anything after scanning QR code')
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 
-msg_box = driver.find_element_by_class_name('_2S1VP')
+msg_box = driver.find_element_by_class_name('_3u328')
 
 print("\nEnter year like 2019\nMonth like 1\nDay like 1\nWhere 1 means jan for month\n")
 
@@ -37,12 +37,12 @@ minu = int(input("Minute of the hour 0-59--> "))
 sec = int(input("Second of hour 0-59--> "))
 
 work_not_done = True
-print("/nScript is running normally/n")
+print("\nScript is running normally\n")
 while work_not_done:
     if year == localtime()[0] and month == localtime()[1] and day == localtime()[2] and hour == localtime()[3] and minu == localtime()[4] and sec == localtime()[5]:
         for i in range(count):
             msg_box.send_keys(msg)
-            button = driver.find_element_by_class_name('_35EW6')
+            button = driver.find_element_by_class_name('_3M-N-')
             button.click()
         work_not_done = False
         print("Your message "+str(msg)+" has been sent to "+str(name)+" "+str(count)+" times successfully.")
